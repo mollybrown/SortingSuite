bcdc599ab11a4e1c4b02f78d3c5b9069e325478a
Molly <mollbrown@gmail.com>
2016-10-07 10:39:27 -0600

    Add files via upload

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,11 +1,9 @@
-require "pry"
+#require "pry"
 
 class InsertionSort
 	def sort(unsorted_letters)
 
 	sorted_letters = []
-
-	
 
 	until unsorted_letters.empty? == true 
 
@@ -14,10 +12,12 @@ class InsertionSort
 
 		#5. Now that we have our first element in the sorted_letters array, iterate through unsorted letter array elements...
 		unsorted_letters.each do |letter|
+			# if first unsorted letter is less than the first sorted letter, remove first element from unsorted array, place into front of sorted array
 			if letter < sorted_letters.first 
-			# remove first element from unsorted array, place into front of sorted array
 			   sorted_letters.unshift(unsorted_letters.shift)
-			
+			# otherwise, iterate through the sorted letters array, looking for a letter/element that is greater than current targeted unsorted letter.
+			# when a match is found, insert the unsorted letter at the index position that we are currently at in the sorted letters array.
+			else 
 			
 			end
 			end
